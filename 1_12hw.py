47c8791334c33d0dffe0869a7ff00a17e17350dc
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-02-02 14:28:35 -0600

    Add files via upload
    1.12 Homework on Zybooks

---
+++ 1_12hw.py
@@ -0,0 +1,12 @@
+#Name: Vu Tran
+#Student ID: 2233511
+
+# This is 1.12 homework on Zybook.
+
+
+userNum = int(input())
+
+userNumSquared = userNum + userNum # Bug here; fix it when instructed
+
+print(userNumSquared, end="") # Output formatting issue here; fix it when instructed
+
