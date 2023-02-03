8ec69c0a8be03194b7ecea51735208d9164d7e51
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-02-03 00:00:29 -0600

    Add files via upload
    2.20 homework on Zybooks

---
+++ 2_20hw.py
@@ -0,0 +1,53 @@
+#Name: Vu Tran
+#Student ID: 2233511
+
+# This is 2.20 homework on Zybooks
+
+#Part 1: Input from user
+print('Enter amount of lemon juice (in cups) :')
+lemon = float(input())
+
+print('Enter amount of water (in cups) :')
+water = float(input())
+
+print('Enter amount of agave nectar (in cups) :')
+agave_nectar = float(input())
+
+print('How many servings does this make?')
+servings = float(input())
+
+#Print the output.
+print("\nLemonade ingredients - yields {:.2f} servings".format(servings))
+print("{:.2f} cups(s) lemon juice" .format(lemon))
+print("{:.2f} cup(s) water".format(water))
+print("{:.2f} cup(s) agave nectar".format(agave_nectar))
+
+#Part 2: Input a new servings and output new ingredients and serving size.
+print('\nHow many servings would you like to make?')
+desired = float(input())
+
+lemon1 = (lemon * desired)/servings
+water1 = (water * desired)/servings
+agave_nectar1 = (agave_nectar * desired)/servings
+
+print("\nLemonade ingredients - yields {:.2f} servings".format(desired))
+print("{:.2f} cups(s) lemon juice" .format(lemon1))
+print("{:.2f} cup(s) water".format(water1))
+print("{:.2f} cup(s) agave nectar".format(agave_nectar1))
+
+#Part 3: Convert cup(s) to gallons
+print("\nLemonade ingredients - yields {:.2f} servings".format(desired))
+print("{:.2f} gallons(s) lemon juice" .format(lemon1/16))
+print("{:.2f} gallons(s) water".format(water1/16))
+print("{:.2f} gallons(s) agave nectar".format(agave_nectar1/16))
+
+
+
+
+
+
+
+
+
+
+
