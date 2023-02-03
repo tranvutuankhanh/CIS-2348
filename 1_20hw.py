65ef173301cf9808e9f03f44c040aaf213451054
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-02-02 23:59:51 -0600

    Add files via upload
    1.20 homework on Zybooks

---
+++ 1_20hw.py
@@ -0,0 +1,28 @@
+#Name: Vu Tran
+#Student ID: 2233511
+
+# This is 1.20 homework on Zybook.
+print('Enter integer: ')
+user_num = int(input())
+
+print('You entered:', user_num)
+
+square = user_num * user_num
+cube = user_num * user_num * user_num
+
+print(f'{user_num} squared is {square}')
+
+print(f'And {user_num} cubed is {cube} !!')
+
+print('Enter another integer: ')
+user_num1 = int(input())
+
+add = user_num + user_num1
+product = user_num * user_num1
+
+print(f'{user_num} + {user_num1} is {add}')
+print(f'{user_num} * {user_num1} is {product}', end="")
+
+
+
+
