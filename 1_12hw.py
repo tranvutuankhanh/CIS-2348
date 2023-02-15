5084b75ed89de53847fbb80ae8759433a32a9595
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-02-14 23:47:32 -0600

    Update 1_12hw.py

--- 1_12hw.py
+++ 1_12hw.py
@@ -6,7 +6,7 @@
 
 userNum = int(input())
 
-userNumSquared = userNum + userNum # Bug here; fix it when instructed
+userNumSquared = userNum * userNum # Bug here; fix it when instructed
 
-print(userNumSquared, end="") # Output formatting issue here; fix it when instructed
+print(userNumSquared) # Output formatting issue here; fix it when instructed
 
