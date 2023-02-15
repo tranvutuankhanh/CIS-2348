8f072d438d7133bad71efb0b642f4f01495d379f
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-02-14 23:48:18 -0600

    Update 1_20hw.py

--- 1_20hw.py
+++ 1_20hw.py
@@ -2,7 +2,7 @@
 #Student ID: 2233511
 
 # This is 1.20 homework on Zybook.
-print('Enter integer: ')
+print('Enter integer:')
 user_num = int(input())
 
 print('You entered:', user_num)
@@ -14,14 +14,14 @@ print(f'{user_num} squared is {square}')
 
 print(f'And {user_num} cubed is {cube} !!')
 
-print('Enter another integer: ')
+print('Enter another integer:')
 user_num1 = int(input())
 
 add = user_num + user_num1
 product = user_num * user_num1
 
 print(f'{user_num} + {user_num1} is {add}')
-print(f'{user_num} * {user_num1} is {product}', end="")
+print(f'{user_num} * {user_num1} is {product}')
 
 
 
