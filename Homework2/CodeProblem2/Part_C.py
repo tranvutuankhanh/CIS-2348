a54a99a3ccc391cd1b810a07f55cc09f6767c515
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-03-08 11:37:07 -0600

    Create Part_C.py

---
+++ Homework2/CodeProblem2/Part_C.py
@@ -0,0 +1,42 @@
+#Name: Vu Tran
+#Student ID: 2233511
+
+# Part C for Code Problem 02
+
+from datetime import datetime
+
+current_date = datetime.now()
+
+# Read input from file
+with open('inputDates.txt') as f:
+    input_lines = f.readlines()
+
+dates = []
+
+for date_str in input_lines:
+    date_str = date_str.strip()
+    if date_str == '-1':
+        break
+
+    month_start = 0
+    month_end = date_str.find(' ')
+    day_start = month_end + 1
+    day_end = date_str.find(',', day_start)
+    year_start = day_end + 2
+    year_end = year_start + 4
+
+    try:
+        month = date_str[month_start:month_end]
+        day = date_str[day_start:day_end]
+        year = date_str[year_start:year_end]
+        date_str = f"{month} {day}, {year}"
+        date = datetime.strptime(date_str, '%B %d, %Y')
+        if date <= current_date:
+            dates.append(date)
+    except ValueError:
+        pass
+
+# Write output to file
+with open('parsedDates.txt', 'w') as f:
+    for date in dates:
+        f.write(date.strftime('%-m/%-d/%Y') + '\n')
