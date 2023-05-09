11368881c476b92d891feaaa8d0a0bc2353926db
Vu Tran <123287686+tranvutuankhanh@users.noreply.github.com>
2023-05-09 11:26:20 -0500

    Update FinalProjectPart2.py
    In Part 1, the main class for inventory management is called Inventory. In Part 2, this class is named ElectronicsInventory. This makes the purpose of the class more explicit in Part 2.
    
    Part 1 has only one class, Inventory, which is responsible for both managing the inventory and generating the inventory reports. In Part 2, there are two classes: ElectronicsInventory for managing the inventory and InventoryReports (which inherits from ElectronicsInventory) for generating the inventory reports. This design separates the concerns and makes the code easier to understand and maintain.
    
    In Part 1, when loading the manufacturer list, damaged items are marked with a 'Yes' string. In Part 2, damaged items are marked with an empty string. This difference might affect how the two scripts handle and filter damaged items.
    
    In Part 2, there is a helper function called format_date_without_leading_zeros() which formats the date without leading zeros in the month and day. This function is used in various parts of the code to format the date consistently. Code 1 doesn't have this helper function, and it formats dates using the strftime() method directly.
    
    In Part 2, there is an additional method called generate_item_type_inventory_csv() in the InventoryReports class that generates a CSV file for each item type in the inventory. This method is not present in Part 1.
    
    Both Part 1 and Part 2 have a method to query the inventory based on user input. However, the implementation of the query_inventory() method in Part 2 is more efficient, as it uses a list comprehension to filter manufacturers and item types from the user's query. Additionally, it uses a more pythonic approach to find the most expensive item and the closest alternative.

--- FinalProjectPart1/FinalProjectInput.py
+++ FinalProjectPart1/FinalProjectInput.py
@@ -4,69 +4,79 @@
 import csv
 from datetime import datetime
 
-
-# Define the ElectronicsInventory class to manage the inventory of electronic items
+# This class is responsible for managing the inventory of electronic items
 class ElectronicsInventory:
-    # Initialize the inventory dictionary and item_types set
     def __init__(self):
-        self.inventory = {}
-        self.item_types = set()
+        self.inventory = {}  # Dictionary to store item data
+        self.item_types = set()  # Set to store unique item types
 
-    # Load the manufacturer list from a CSV file and update the inventory dictionary and item_types set
+    # Load manufacturer list and store item details in the inventory dictionary
     def load_manufacturer_list(self, filename):
-        with open(filename, 'r') as file:
+        with open(filename, 'r', newline='', encoding='utf-8') as file:
             reader = csv.reader(file)
             for row in reader:
-                item_id, manufacturer, item_type, damaged = row[0], row[1], row[2], row[3] if len(
+                item_id, manufacturer, item_type, damaged = row[0], row[1].strip(), row[2], row[3] if len(
                     row) == 4 else ''
                 self.inventory[item_id] = {'manufacturer': manufacturer, 'item_type': item_type, 'damaged': damaged}
                 self.item_types.add(item_type)
 
-    # Load the price list from a CSV file and update the inventory dictionary with the price information
+    # Load price list and update the inventory dictionary with price information
     def load_price_list(self, filename):
-        with open(filename, 'r') as file:
+        with open(filename, 'r', newline='', encoding='utf-8') as file:
             reader = csv.reader(file)
             for row in reader:
                 item_id, price = row[0], float(row[1])
                 if item_id in self.inventory:
                     self.inventory[item_id]['price'] = price
 
-    # Load the service dates list from a CSV file and update the inventory dictionary with the service date information
+    # Load service dates list and update the inventory dictionary with service date information
     def load_service_dates_list(self, filename):
-        with open(filename, 'r') as file:
+        with open(filename, 'r', newline='', encoding='utf-8') as file:
             reader = csv.reader(file)
             for row in reader:
                 item_id, service_date = row[0], datetime.strptime(row[1], '%m/%d/%Y')
                 if item_id in self.inventory:
                     self.inventory[item_id]['service_date'] = service_date
 
+# This class is responsible for generating various inventory reports by extending the ElectronicsInventory class
+class InventoryReports(ElectronicsInventory):
     # Generate a full inventory CSV file sorted by manufacturer and item ID
     def generate_full_inventory_csv(self):
         def sort_key(inventory_item):
             inventory_id, inventory_data = inventory_item
             return inventory_data['manufacturer'], inventory_id
 
+        def format_date_without_leading_zeros(date):
+            return f"{date.month}/{date.day}/{date.year}"
+
         with open('FullInventory.csv', 'w', newline='') as file:
             writer = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar=' ')
             for item_id, item in sorted(self.inventory.items(), key=sort_key):
-                service_date = item['service_date'].strftime('%-m/%-d/%Y') if 'service_date' in item else ''
+                service_date = format_date_without_leading_zeros(item['service_date']) if 'service_date' in item else ''
                 writer.writerow([item_id, item['manufacturer'], item['item_type'], int(item['price']),
                                  service_date, item['damaged']])
 
     # Generate a laptop inventory CSV file sorted by item ID
     def generate_laptop_inventory_csv(self):
+        def format_date_without_leading_zeros(date):
+            return f"{date.month}/{date.day}/{date.year}"
+
         with open('LaptopInventory.csv', 'w', newline='') as file:
             writer = csv.writer(file)
             for item_id, item in sorted(self.inventory.items()):
                 if item['item_type'] == 'laptop':
                     damaged = item['damaged'] if item['damaged'] else ''
                     price = int(item['price']) if 'price' in item else ''
-                    service_date = item['service_date'].strftime('%-m/%-d/%Y') if 'service_date' in item else ''
+                    service_date = format_date_without_leading_zeros(
+                        item['service_date']) if 'service_date' in item else ''
                     writer.writerow([item_id, item['manufacturer'], price, service_date, damaged])
 
     # Generate a CSV file with items that have a service date in the past, sorted by service date
     def generate_past_service_date_inventory_csv(self):
         past_service_items = []
+
+        def format_date_without_leading_zeros(date):
+            return f"{date.month}/{date.day}/{date.year}"
 
         for past_item_id, past_item in self.inventory.items():
             if 'service_date' in past_item and past_item['service_date'] < datetime.now():
@@ -86,7 +96,7 @@ class ElectronicsInventory:
             for past_item_id, past_item in past_service_items:
                 writer.writerow(
                     [past_item_id, past_item['manufacturer'], past_item['item_type'], int(past_item['price']),
-                     past_item['service_date'].strftime('%-m/%-d/%Y'), past_item['damaged']])
+                     format_date_without_leading_zeros(past_item['service_date']), past_item['damaged']])
 
     # Generate a damaged inventory CSV file sorted by descending price
     def generate_damaged_inventory_csv(self):
@@ -94,65 +104,107 @@ class ElectronicsInventory:
             inventory_id, inventory_data = inventory_item
             return -inventory_data['price']
 
+        def format_date_without_leading_zeros(date):
+            return f"{date.month}/{date.day}/{date.year}"
+
         with open('DamagedInventory.csv', 'w', newline='') as file:
             writer = csv.writer(file, quoting=csv.QUOTE_NONE, escapechar=' ')
             for item_id, item in sorted(self.inventory.items(), key=sort_key):
                 if item['damaged']:
                     writer.writerow([item_id, item['manufacturer'], item['item_type'], int(item['price']),
-                                     item['service_date'].strftime('%-m/%-d/%Y')])
+                                     format_date_without_leading_zeros(item['service_date'])])
 
-    def generate_tower_inventory_csv(self):
-        with open('TowerInventory.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            # Loop over the inventory items sorted by ID
-            for item_id, item in sorted(self.inventory.items()):
-                # Write to the CSV file only if the item is a tower
-                if item['item_type'] == 'tower':
-                    # Extract the relevant data from the item dictionary
-                    damaged = item.get('damaged', '')
-                    price = int(item['price']) if 'price' in item else ''
-                    service_date = item.get('service_date', '').strftime('%-m/%-d/%Y')
-                    # Write a new row to the CSV file with the extracted data
-                    writer.writerow([item_id, item['manufacturer'], price, service_date, damaged])
+    # Generate a CSV file for each item type in the inventory
+    def generate_item_type_inventory_csv(self):
+        def format_date_without_leading_zeros(date):
+            return f"{date.month}/{date.day}/{date.year}"
 
-    def generate_phone_inventory_csv(self):
-        with open('PhoneInventory.csv', 'w', newline='') as file:
-            writer = csv.writer(file)
-            # Loop over the inventory items sorted by ID
-            for item_id, item in sorted(self.inventory.items()):
-                # Write to the CSV file only if the item is a phone
-                if item['item_type'] == 'phone':
-                    # Extract the relevant data from the item dictionary
-                    damaged = item.get('damaged', '')
-                    price = int(item['price']) if 'price' in item else ''
-                    service_date = item.get('service_date', '').strftime('%-m/%-d/%Y')
-                    # Write a new row to the CSV file with the extracted data
-                    writer.writerow([item_id, item['manufacturer'], price, service_date, damaged])
+        for item_type in self.item_types:
+            with open(f'{item_type.capitalize()}Inventory.csv', 'w', newline='') as file:
+                writer = csv.writer(file)
+                for item_id, item in sorted(self.inventory.items()):
+                    if item['item_type'] == item_type:
+                        damaged = item.get('damaged', '')
+                        price = int(item['price']) if 'price' in item else ''
+                        service_date = format_date_without_leading_zeros(
+                            item['service_date']) if 'service_date' in item else ''
+                        writer.writerow([item_id, item['manufacturer'], price, service_date, damaged])
 
+    # Query the inventory based on the user's input and return the most expensive item and the closest alternative
+    def query_inventory(self, query):
+        query_words = [word for word in query.strip().lower().split() if word != 'a']
 
-# The main function to run the script
+        manufacturers = [word for word in query_words if any(
+            word.lower() in inventory_item['manufacturer'].lower() for _, inventory_item in self.inventory.items())]
+        item_type = next((word for word in query_words if any(
+            word.lower() == inventory_item['item_type'].lower() for _, inventory_item in self.inventory.items())),
+                         '')
+
+        if len(manufacturers) > 1:
+            manufacturers = list(set(manufacturers))
+            if len(manufacturers) > 1:
+                return "No such item in inventory"
+
+        manufacturer = manufacturers[0] if manufacturers else ''
+
+        valid_items = []
+        for item_id, item in self.inventory.items():
+            if item_type.lower() == item['item_type'].lower():
+                if item.get('damaged', False):
+                    continue
+                elif 'service_date' in item and item['service_date'] < datetime.now():
+                    continue
+                else:
+                    valid_items.append((item_id, item))
+
+        if not valid_items:
+            return "No such item in inventory"
+
+        # Find the most expensive item from the specified manufacturer
+        most_expensive_item = None
+        for item in valid_items:
+            if manufacturer.lower() in item[1]['manufacturer'].lower():
+                if most_expensive_item is None or item[1]['price'] > most_expensive_item[1]['price']:
+                    most_expensive_item = item
+
+        if most_expensive_item is None:
+            return f"No {manufacturer} {item_type} in inventory"
+
+        # Find the closest priced item from a different manufacturer
+        closest_item = None
+        min_price_diff = float('inf')
+        for item in valid_items:
+            if item[1]['manufacturer'].lower() != most_expensive_item[1]['manufacturer'].lower():
+                price_diff = abs(item[1]['price'] - most_expensive_item[1]['price'])
+                if price_diff < min_price_diff:
+                    min_price_diff = price_diff
+                    closest_item = item
+
+        result = f"Your item is: {most_expensive_item[0]}, {most_expensive_item[1]['manufacturer']}, {most_expensive_item[1]['item_type']}, {int(most_expensive_item[1]['price'])}"
+
+        if closest_item:
+            result += f"\nYou may, also, consider: {closest_item[0]}, {closest_item[1]['manufacturer']}, {closest_item[1]['item_type']}, {int(closest_item[1]['price'])} "
+
+        return result
+
+# Main function to run the inventory application
 def main():
-    inventory = ElectronicsInventory()
+    inventory = InventoryReports()
 
     manufacturer_list_file = "ManufacturerList.csv"
     price_list_file = "PriceList.csv"
     service_dates_list_file = "ServiceDatesList.csv"
-
-    # Load the manufacturer, price, and service dates lists
     inventory.load_manufacturer_list(manufacturer_list_file)
     inventory.load_price_list(price_list_file)
     inventory.load_service_dates_list(service_dates_list_file)
 
-    # Generate the full inventory, laptop inventory, past service date inventory, damaged inventory,
-    # Tower inventory, and Phone inventory CSV files
-    inventory.generate_full_inventory_csv()
-    inventory.generate_laptop_inventory_csv()
-    inventory.generate_past_service_date_inventory_csv()
-    inventory.generate_damaged_inventory_csv()
-    inventory.generate_tower_inventory_csv()
-    inventory.generate_phone_inventory_csv()
+    while True:
+        query = input("\nPlease enter the manufacturer and item type (or 'q' to quit): ")
+        if query.strip().lower() == 'q':
+            break
+
+        print(inventory.query_inventory(query))
 
 
-# Run the main function if the script is run as the main module
 if __name__ == "__main__":
     main()
